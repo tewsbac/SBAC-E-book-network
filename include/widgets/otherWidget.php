70b0ba38173bff3f587fa341e07d92efba5d8982
tewsbac <108539152+tewsbac@users.noreply.github.com>
2022-07-01 23:58:48 +0700

    Add files via upload

---
+++ include/widgets/otherWidget.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('WEB_ROOT')) {
+	exit;
+}
+?>
+<div class="panel panel-info">
+  <div class="panel-heading">widget</div>
+  <div class="panel-body">
+		[WIDGET]
+  </div>
+</div>
---
+++ include/widgets/widget2.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('WEB_ROOT')) {
+	exit;
+}
+?>
+<div class="panel panel-info">
+  <div class="panel-heading">widget2</div>
+  <div class="panel-body">
+		[WIDGET]
+  </div>
+</div>
